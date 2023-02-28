a957e1093b9a016f6eee354f74e5712e6e8b83fb
Rohan Kumar <rohan@emsec.uk>
2023-02-28 18:11:11 +0530

    comment all logs in SPA

--- Spa.py
+++ Spa.py
@@ -8,7 +8,7 @@ from databaseConnection import collection2
 import time
 from startDisplay import *
 from driverpath import torPath
-from flag import sendLog,sendData
+# from flag import sendLog,sendData
 
 def scroll(driver):
     reached_page_end = False
@@ -17,13 +17,13 @@ def scroll(driver):
         driver.find_element(By.XPATH,'//body').send_keys(Keys.END)   
         time.sleep(15)
         print("Website is Scrolling now...")
-        sendLog("Website is Scrolling now...")
+        # sendLog("Website is Scrolling now...")
         
         new_height = driver.execute_script("return document.body.scrollHeight")
         if last_height == new_height:
                 reached_page_end = True
                 print("Page ended...Scrolling Done!!")
-                sendLog("Page ended...Scrolling Done!!")
+                # sendLog("Page ended...Scrolling Done!!")
                 
         else:
                 last_height = new_height
@@ -36,21 +36,21 @@ def press_next_btn(driver,xpath_of_next_btn) :
             driver.execute_script("arguments[0].scrollIntoView();", next_btn)
             time.sleep(8)
             print("Opening next page...")
-            sendLog("Opening next page...")
+            # sendLog("Opening next page...")
             next_btn.click()
             return True
         else :
             print("Reached at last page")
-            sendLog("Reached at last page")
+            # sendLog("Reached at last page")
             return False
 
     except NoSuchElementException:
         print("Reached at last page")
-        sendLog("Reached at last page")
+        # sendLog("Reached at last page")
         return False
     except ElementNotInteractableException:
         print("Reached at last page")
-        sendLog("Reached at last page")
+        # sendLog("Reached at last page")
         return False
 
 def no_next_btn(driver,i,xpath_of_pagination_container,tag_name_of_pages) :
@@ -59,18 +59,18 @@ def no_next_btn(driver,i,xpath_of_pagination_container,tag_name_of_pages) :
     all_pages = pagination_container.find_elements(By.TAG_NAME,str(tag_name_of_pages))
     if len(all_pages) == i :
         print("Reached at last page!!")
-        sendLog("Reached at last page!!")
+        # sendLog("Reached at last page!!")
         return False
     next_page = all_pages[i]
     driver.execute_script("arguments[0].scrollIntoView();", next_page)
     print("Opening next page...please wait...")
-    sendLog("Opening next page...please wait...")
+    # sendLog("Opening next page...please wait...")
     next_page.click()
     return True                
 
 def scrap(driver,iterator,title_xpath,body_xpath,date_xpath,link):
     print("Data scraping....")
-    sendLog("Data scraping....")
+    # sendLog("Data scraping....")
     data = driver.find_elements(By.XPATH, iterator)
     for d in data:
         temp = ''
@@ -109,17 +109,17 @@ def scrap(driver,iterator,title_xpath,body_xpath,date_xpath,link):
             date='Not found'
 
         print("Data scrapped!!")    
-        sendLog("Data scrapped")    
+        # sendLog("Data scrapped")    
          
 
         if title !='Not found':
             print("Data storing in DB....")        
-            sendLog("Data storing in DB....")        
+            # sendLog("Data storing in DB....")        
             db_dict = {'Title': title, 'Body': body_data, 'Date': date, 'Url': link}
             existing_data = collection2.find_one({'Title': title})
 
             print(db_dict)
-            sendData(db_dict)
+            # sendData(db_dict)
             if existing_data:
                 if existing_data['Body'] != body_data or existing_data['Date'] != date:
                     update_query = {'$set': {'Body': body_data, 'Date': date, 'Url': link}}
@@ -127,45 +127,45 @@ def scrap(driver,iterator,title_xpath,body_xpath,date_xpath,link):
             else:
                 collection2.insert_one(db_dict)
             print("Database Updated!!")    
-            sendLog("Database Updated!!")    
+            # sendLog("Database Updated!!")    
                
         else:
             print("Data not fetched!!")            
-            sendLog("Data not fetched!!")            
+            # sendLog("Data not fetched!!")            
             
 def spa(darkweb_url, iterator, title_xpath, body_xpath,date_xpath=None,scrollable=False,clickable=False,clickable_btn_xpath=None,pagination = False,is_nextbtn=True,xpath_of_next_btn=None,xpath_of_pagination_container=None,tag_name_of_pages=None,waitTime=10):
     xvfb_display = start_xvfb()
     with TorBrowserDriver(torPath) as driver:
         driver.maximize_window()
         print("Website is opening....")
-        sendLog("Website is opening....")
+        # sendLog("Website is opening....")
         driver.get(darkweb_url)
         print("Waiting....")
-        sendLog("Waiting....")
+        # sendLog("Waiting....")
 
         print("Sleep time is ",waitTime," sec")
-        # sendLog("Sleep time is ",waitTime," sec")
+        ## sendLog("Sleep time is ",waitTime," sec")  #test
 
         time.sleep(waitTime)
         print("Site opened!!")
-        sendLog("Site opened!!")
+        # sendLog("Site opened!!")
         
 
         if scrollable==True:
             print("Site is scrollable...please wait")
-            sendLog("Site is scrollable...please wait")
+            # sendLog("Site is scrollable...please wait")
             scroll(driver)
 
         while clickable == True :
             print("Site is clickable...")
-            sendLog("Site is clickable")
+            # sendLog("Site is clickable")
             if clickable_btn_xpath!=None:
                 clickable = press_next_btn(driver,clickable_btn_xpath)
             
 
         if pagination == True:
             print("Site contains pagination...")
-            sendLog("Site contains pagination")
+            # sendLog("Site contains pagination")
             i=0
             while pagination == True :
                 scrap(driver,iterator,title_xpath,body_xpath,date_xpath,darkweb_url)
